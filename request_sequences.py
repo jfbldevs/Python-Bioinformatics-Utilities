a158ac9725976d534f6c1be1ebeb5772c03d8883
Jorge F. Beltrán <70285859+jfbldevs@users.noreply.github.com>
2021-11-03 14:44:27 -0300

    Update request_sequences.py

--- request_sequences.py
+++ request_sequences.py
@@ -1,7 +1,7 @@
 secuencias = ['P81136', 'A5U0G8', 'A5I7X8',]
 
 for i in secuencias:
-      import requests
+  import requests
   #params = {"query": "P00750", "format": "fasta", "include": "yes"}
   params = {"query": i, "format": "fasta"}
   response = requests.get("http://www.uniprot.org/uniprot/", params=params)
