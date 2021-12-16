1d5aa0882b9e8ff73f3a19d756f266b69f555656
Jorge F. Beltrán <70285859+jfbldevs@users.noreply.github.com>
2021-12-16 19:23:40 -0300

    Add files via upload

---
+++ sequence_request_ncbi.py
@@ -0,0 +1,16 @@
+#pip install dendropy
+#Own customization
+from urllib.parse import scheme_chars
+from dendropy.interop import genbank
+gb_dna = genbank.GenBankProtein(ids=["XP_015198995.1",
+"XP_015198998.1",
+"XP_015199004.1",
+"XP_015198997.1",
+"XP_041091045.1"]
+)
+#symbol = 0
+for gb in gb_dna:
+  #symbol += 1
+  print(f">{gb.definition}")
+  print(gb.sequence_text.upper())
+
