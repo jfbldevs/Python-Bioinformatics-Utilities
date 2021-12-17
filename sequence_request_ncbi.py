8c01143793508e3dfc3d1b80bc585faebc36fd7d
Dr. Jorge F. Beltrán <70285859+jfbldevs@users.noreply.github.com>
2021-12-16 23:11:02 -0300

    Update sequence_request_ncbi.py

--- sequence_request_ncbi.py
+++ sequence_request_ncbi.py
@@ -2,14 +2,14 @@
 #Own customization
 from urllib.parse import scheme_chars
 from dendropy.interop import genbank
-gb_dna = genbank.GenBankProtein(ids=["XP_015198995.1",
+gb_pep = genbank.GenBankProtein(ids=["XP_015198995.1",
 "XP_015198998.1",
 "XP_015199004.1",
 "XP_015198997.1",
 "XP_041091045.1"]
 )
 #symbol = 0
-for gb in gb_dna:
+for gb in gb_pep:
   #symbol += 1
   print(f">{gb.definition}")
   print(gb.sequence_text.upper())
